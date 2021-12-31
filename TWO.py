f54a00f298a6bc9ec55c6a4f66ae99e5d43783c7
operator001-umbrella-one <96925030+operator001-umbrella-one@users.noreply.github.com>
2021-12-31 21:14:41 +0100

    Add files via upload

---
+++ TWO.py
@@ -0,0 +1,7 @@
+from pyfiglet import figlet_format
+
+print(figlet_format("Umbrella ONE", font="standard"))
+
+print("--------------------------------------------------------------")
+
+print("You have already arrived here! Bravo! \nBut this is nothing in the future. Patience is a virtue , watch TWO, it could change.\n Thank you, cordially operator #002, Umbrella One.")
